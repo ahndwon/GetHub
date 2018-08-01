904d69e83ba55763ab9fdf216ac287c4bcd0d084
Dongwon Ahn <gahlok24@gmail.com>
2018-08-01 19:59:53 +0900

    added contributions graph in OverView Tab

--- app/src/main/java/com/ahndwon/gethub/ui/fragment/profile/OverViewFragment.kt
+++ app/src/main/java/com/ahndwon/gethub/ui/fragment/profile/OverViewFragment.kt
@@ -1,5 +1,7 @@
 package com.ahndwon.gethub.ui.fragment.profile
 
+import android.graphics.drawable.PictureDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -7,18 +9,29 @@ import android.view.View
 import android.view.ViewGroup
 import com.ahndwon.gethub.R
 import com.ahndwon.gethub.utils.GlideApp
+import com.ahndwon.gethub.utils.SvgSoftwareLayerSetter
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import kotlinx.android.synthetic.main.fragment_over_view.view.*
 
 
 class OverViewFragment : Fragment() {
+
+//    private var requestBuilder: RequestBuilder<PictureDrawable>? = null
+
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_over_view, container, false)
 
-        GlideApp.with(this)
-                .load("http://ghchart.rshah.org/ahndwon")
-                .placeholder(R.drawable.ic_github)
-                .into(view.chart)
+        val uri = Uri.parse("https://ghchart.rshah.org/ahndwon")
+
+        val requestBuilder = GlideApp.with(this)
+                .`as`(PictureDrawable::class.java)
+                .error(R.drawable.ic_issue_opened)
+                .transition(withCrossFade())
+                .listener(SvgSoftwareLayerSetter())
+
+        requestBuilder.load(uri).into(view.chart);
+
         return view
     }
 }
--- app/src/main/java/com/ahndwon/gethub/utils/GlideModule.kt
+++ app/src/main/java/com/ahndwon/gethub/utils/GlideModule.kt
@@ -2,6 +2,6 @@ package com.ahndwon.gethub.utils
 
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class MyAppGlideModule: AppGlideModule()
+//
+//@GlideModule
+//class MyAppGlideModule: AppGlideModule()
